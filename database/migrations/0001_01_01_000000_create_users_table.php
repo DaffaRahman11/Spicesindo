1a53a11e41cc4bdcd20f467709596882af75a2f0
Daffa Fauzi Rahman <124863150+DaffaRahman11@users.noreply.github.com>
2025-06-03 17:39:01 +0700

    Update 0001_01_01_000000_create_users_table.php
    ubah full name

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('FirstName');
             $table->string('LastName');
-            // $table->string('FullName');
+            $table->string('FullName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
